8d518fbd302901e02750bca8724bc81656dd2299
louisadayton <34587387+louisadayton@users.noreply.github.com>
2018-05-17 12:19:21 -0600

    Add files via upload

---
+++ arff.py
@@ -0,0 +1,4 @@
+#this code will hopefully convert panda frames to arff files in three steps
+#1. Get the pieces of data
+#2. Manipulate the data to put it in arff format
+#3. Read the data into a file
