241fbdb7d4834a896230e22e0bba8bced45449ce
Louisa Dayton <dayton.louisa@gmail.com>
2018-05-17 12:39:19 -0600

    First Draft

--- arff.py
+++ arff.py
@@ -1,4 +1,28 @@
-#this code will hopefully convert panda frames to arff files in three steps
-#1. Get the pieces of data
-#2. Manipulate the data to put it in arff format
-#3. Read the data into a file
+import pandas as pd
+
+
+def isNumeric(column): 
+    for item in column:
+        if type(item) != float and type(item) != int:
+            return False   
+    return True
+
+df = pd.read_table("cool_file.txt")
+
+writeFile = open("output.txt", 'w')
+
+for colName in list(df):		
+    writeFile.write("@ATTRIBUTE\t" + colName)
+    options = set([])
+    if isNumeric(df[colName]) == True:
+        writeFile.write("\tNUMERIC\n") 
+    else:
+        writeFile.write("\t{")
+        for value in df[colName]:
+            options.add(value)
+        writeFile.write(str(options.pop()))
+        for o in options:
+            writeFile.write("," + str(o))
+        writeFile.write("}\n")
+writeFile.write("\n") 
+writeFile.close() 
