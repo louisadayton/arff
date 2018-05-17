1fc59c9c01d37eadde823b3fbf0349f4ed9123b7
louisadayton <34587387+louisadayton@users.noreply.github.com>
2018-05-17 12:29:17 -0600

    Delete arff.py

--- arff.py
+++
@@ -1,4 +0,0 @@
-#this code will hopefully convert panda frames to arff files in three steps
-#1. Get the pieces of data
-#2. Manipulate the data to put it in arff format
-#3. Read the data into a file
