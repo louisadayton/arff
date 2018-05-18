9d68361b556475ed73736eb32cccb4f7c68362bc
Louisa Dayton <dayton.louisa@gmail.com>
2018-05-18 13:48:42 -0600

    Still have to work out @relation

--- arff.py
+++ arff.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import re
 
 def isNumeric(column): 
     for item in column:
@@ -8,21 +8,32 @@ def isNumeric(column):
     return True
 
 df = pd.read_table("cool_file.txt")
-
-writeFile = open("output.txt", 'w')
-
+fileName = "output.txt"
+writeFile = open(fileName, 'w')
+pattern = r"\.*"
+relation = fileName.rstrip(pattern)
+writeFile.write("@RELATION\t" + relation + "\n")
 for colName in list(df):		
     writeFile.write("@ATTRIBUTE\t" + colName)
     options = set([])
     if isNumeric(df[colName]) == True:
         writeFile.write("\tNUMERIC\n") 
-    else:
-        writeFile.write("\t{")
+    else: 
         for value in df[colName]:
             options.add(value)
-        writeFile.write(str(options.pop()))
-        for o in options:
-            writeFile.write("," + str(o))
-        writeFile.write("}\n")
-writeFile.write("\n") 
+        if len(options) == len(df[colName]):
+            writeFile.write("\tstring\n")
+        else:
+            writeFile.write("\t{")
+            writeFile.write(str(options.pop()))
+            for o in options:
+                writeFile.write("," + str(o))
+            writeFile.write("}\n")
+writeFile.write("@DATA\n")
+for line in df.values:
+    for word in line:
+        writeFile.write(str(word) + ",")
+    writeFile.write("\n")
+ 
+ 
 writeFile.close() 
