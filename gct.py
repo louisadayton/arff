03d69b31e53135e30c4744ddb6bb978e76cc255f
Louisa Dayton <dayton.louisa@gmail.com>
2018-05-24 13:14:02 -0600

    Turned into a function

--- gct.py
+++ gct.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
-df = pd.read_table("allaml.dataset.gct.txt")
-df = df.fillna("")
-fileName = "output.gct"
-
-writeFile = open(fileName, 'w')
-writeFile.write("#1.2\n")
-writeFile.write(str(len(df.index))) 
-writeFile.write("\t" + str(len(df.columns[2:])) + "\n")
-for col in df.columns:
-    writeFile.write(str(col) + "\t")
-writeFile.write("\n")
-for line in df.values:
-    for word in line:
-        writeFile.write(str(word) + "\t")
+def toGCT(df, fileName):
+    df = df.fillna("") 
+#Write #1.2
+    writeFile = open(fileName, 'w')
+    writeFile.write("#1.2\n")
+#Write the number of rows and the number of columns
+    writeFile.write(str(len(df.index))) 
+    writeFile.write("\t" + str(len(df.columns[2:])) + "\n")
+    for col in df.columns:
+        writeFile.write(str(col) + "\t")
     writeFile.write("\n")
-writeFile.close()	
+    for line in df.values:
+        for word in line:
+            writeFile.write(str(word) + "\t")
+        writeFile.write("\n")
+    writeFile.close()	
