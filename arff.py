81d0f9001c95066608b1becb3fb1c823d0558ef4
Louisa Dayton <dayton.louisa@gmail.com>
2018-05-24 11:10:19 -0600

    Made toARFF function

--- arff.py
+++ arff.py
@@ -19,51 +19,49 @@ def isDate(column):
                 return False
     return True
 
-#This is the stuff that will need to be tweaked
-#Right now I read in a file as a pandas dataframe
-df = pd.read_table("cool_file.txt")
-df = df.fillna("?")
-fileName = "output.arff"
+def toARFF(df, fileName):
+   
+    df = df.fillna("?")
 
-writeFile = open(fileName, 'w')
-pattern = r"\..*"
-relation = re.sub(pattern, "", fileName)
-writeFile.write("@RELATION\t" + relation + "\n")
+    writeFile = open(fileName, 'w')
+    pattern = r"\..*"
+    relation = re.sub(pattern, "", fileName)
+    writeFile.write("@RELATION\t" + relation + "\n")
 #Check the type for each column and write them in as attributes
-for colName in list(df):		
-    writeFile.write("@ATTRIBUTE\t" + colName)
-    options = set([])
-    if isNumeric(df[colName]) == True:
-        writeFile.write("\tNUMERIC\n")
-    elif isDate(df[colName]) == True:
-        writeFile.write("\tDATE\tyyyy-MM-dd\n") 
-        for value in df[colName]:
-            fullDate = pd.to_datetime(value, errors = 'ignore')
-            date = str(fullDate).split(" ")[0] 
-            df = df.replace(value, date) 
-    else: 
-        for value in df[colName]:
-            if value != "?": 
-                options.add(value)
-        if len(options) == len(df[colName]):
-            writeFile.write("\tstring\n")
-        else:
-            writeFile.write("\t{")
-            writeFile.write(str(options.pop()))
-            for o in options:
-                writeFile.write("," + str(o))
-            writeFile.write("}\n")
+    for colName in list(df):		
+        writeFile.write("@ATTRIBUTE\t" + colName)
+        options = set([])
+        if isNumeric(df[colName]) == True:
+            writeFile.write("\tNUMERIC\n")
+        elif isDate(df[colName]) == True:
+            writeFile.write("\tDATE\tyyyy-MM-dd\n") 
+            for value in df[colName]:
+                fullDate = pd.to_datetime(value, errors = 'ignore')
+                date = str(fullDate).split(" ")[0] 
+                df = df.replace(value, date) 
+        else: 
+            for value in df[colName]:
+                if value != "?": 
+                    options.add(value)
+            if len(options) == len(df[colName]):
+                writeFile.write("\tstring\n")
+            else:
+                writeFile.write("\t{")
+                writeFile.write(str(options.pop()))
+                for o in options:
+                    writeFile.write("," + str(o))
+                writeFile.write("}\n")
 #Write out the data
 #If there's a space in the value, surround it with quotes
-writeFile.write("@DATA\n")
-for line in df.values:
-    writeFile.write(str(line[0]))
-    for i in range(1, len(line)):
-        if " " in str(line[i]):
-            writeFile.write(",'" + str(line[i]) + "'")
-            continue 
-        writeFile.write("," + str(line[i])) 
-    writeFile.write("\n")
+    writeFile.write("@DATA\n")
+    for line in df.values:
+        writeFile.write(str(line[0]))
+        for i in range(1, len(line)):
+            if " " in str(line[i]):
+                writeFile.write(",'" + str(line[i]) + "'")
+                continue 
+            writeFile.write("," + str(line[i])) 
+        writeFile.write("\n")
  
  
-writeFile.close() 
+    writeFile.close() 
